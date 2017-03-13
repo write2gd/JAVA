d14cf5465a9d82a0f015d18878e224318042a89b
dasg <dasg@DGC001156509.ap.cat.com>
2017-03-13 15:16:47 +0530

    ManytoManyUnidirectional Association

---
+++ ManyToManyUniDirectional/src/main/java/com/gd/demo/manytomanyunidirectional/annotation/Course.java
@@ -0,0 +1,45 @@
+package com.gd.demo.manytomanyunidirectional.annotation;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ *
+ * @author Gouranga Das
+ */
+@Entity(name = "Course")
+@Table(name = "COURSES")
+public class Course implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "COURSE_ID")
+    private long id;
+
+    @Column(name = "COURSE_NAME")
+    private String courseName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+ 
+}
